a30baed1ff037d5ddbb2862d20bd451fef1e6e64
LinqWithQ <linqherng2005@gmail.com>
2026-01-23 15:24:17 +0800

    add and list function

--- src/main/java/Qlin.java
+++ src/main/java/Qlin.java
@@ -12,12 +12,30 @@ public class Qlin {
         */
         Scanner sc = new Scanner(System.in);
 
-         String greeting = "Hello!, I'm Qlin.\n"
-                 + "What can I do for you?\n";
-         System.out.println(greeting);
+        String[] store = new String[101];
+        int counter = 0;
 
-         String input = sc.nextLine();
+        String greeting = "Hello!, I'm Qlin.\n"
+            + "What can I do for you?\n";
 
-         if (input.equals("bye")) System.out.println("Goodbye, hope to not see you again!");
+        System.out.println(greeting);
+
+        boolean terminate = false;
+        while (!terminate) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Goodbye, hope to not see you again!");
+                terminate = true;
+            } else if (input.equals("list")) {
+                for (int x = 0; x < counter; x++) {
+                    System.out.println((x + 1) + ". " + store[x]);
+                }
+            } else {
+                System.out.println("added: " + input);
+                store[counter] = input;
+                counter++;
+            }
+        }
     }
 }
