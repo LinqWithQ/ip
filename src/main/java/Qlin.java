4ebf7416a0058ea97d3b6a7aa9803ad584f5860b
LinqWithQ <linqherng2005@gmail.com>
2026-01-25 20:30:09 +0800

    add command for mark and unmark

--- src/main/java/Qlin.java
+++ src/main/java/Qlin.java
@@ -4,32 +4,47 @@ public class Qlin {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         Task[] tasks = new Task[101];
         int counter = 0;
-
-        String greeting = "Hello!, I'm Qlin.\n"
-            + "What can I do for you?\n";
-
+        String greeting = "Hello!, I'm Qlin.\n" + "What can I do for you?\n";
         System.out.println(greeting);
-
         boolean terminate = false;
+
         while (!terminate) {
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
+
                 System.out.println("Goodbye, hope to not see you again!");
                 terminate = true;
-            } else if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
 
+            } else if (input.equals("list")) {
+
+                System.out.println("Here are the tasks in your list:");
                 for (int x = 0; x < counter; x++) {
                     System.out.println((x + 1) + ". " + tasks[x].toString());
                 }
+
+            } else if (input.startsWith("mark")) {
+
+                int index = Integer.parseInt(input.substring(4).trim());
+                tasks[index].setDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[index].toString());
+
+            } else if (input.startsWith("unmark")) {
+
+                int index = Integer.parseInt(input.substring(6).trim());
+                tasks[index].unDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[index].toString());
+
             } else {
+
                 System.out.println("added: " + input);
                 tasks[counter] = new Task(input);
                 counter++;
+
             }
         }
     }
