d183a7c421dce659a005ac4f1223e64bd8945217
LinqWithQ <linqherng2005@gmail.com>
2026-01-26 00:06:42 +0800

    root exception for deadline

---
+++ src/main/java/InvalidDeadlineException.java
@@ -0,0 +1,7 @@
+public class InvalidDeadlineException extends QlinException{
+    @Override
+    public void echo() {
+        System.out.println("Sry, your input format is wrong");
+        System.out.println("Consider this: deadline <name> <starting time>");
+    }
+}
