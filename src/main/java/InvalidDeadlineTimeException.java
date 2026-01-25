22857238858d95ffdda571a173d8f51b71101b85
LinqWithQ <linqherng2005@gmail.com>
2026-01-26 01:27:37 +0800

    a special case for invalid deadline exception

---
+++ src/main/java/InvalidDeadlineTimeException.java
@@ -0,0 +1,6 @@
+public class InvalidDeadlineTimeException extends InvalidDeadlineException{
+    @Override
+    public void echo() {
+        System.out.println("Sry, deadline time cannot be empty.");
+    }
+}
