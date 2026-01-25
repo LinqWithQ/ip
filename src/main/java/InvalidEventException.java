b08adfedd942d5dbc1722167f04fc13d1649301b
LinqWithQ <linqherng2005@gmail.com>
2026-01-26 01:03:27 +0800

    updated the message to be more corrrect

--- src/main/java/InvalidEventException.java
+++ src/main/java/InvalidEventException.java
@@ -2,6 +2,6 @@ public class InvalidEventException extends QlinException{
     @Override
     public void echo() {
         System.out.println("Sry, your input format is invalid");
-        System.out.println("Consider this: event <name> <starting time> <ending time>");
+        System.out.println("Consider this: event <name> /<starting time> /<ending time>");
     }
 }
