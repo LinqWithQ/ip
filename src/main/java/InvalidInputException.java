4a2609512e9eaaf0646138a27a5fcf7be47294e0
LinqWithQ <linqherng2005@gmail.com>
2026-01-26 15:05:41 +0800

    provide valid inputs

--- src/main/java/InvalidInputException.java
+++ src/main/java/InvalidInputException.java
@@ -2,5 +2,6 @@ public class InvalidInputException extends QlinException{
     @Override
     public void echo() {
         System.out.println("Sry, your input is invalid");
+        System.out.println("Valid commands: \"list\", \"mark\", \"unmark\", \"todo\", \"deadline\", \"event\", \"task\", \"delete\"");
     }
 }
