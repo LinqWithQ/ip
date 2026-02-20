dac9613d5f988777b998b7164a82983c1aa6dccb
LinqWithQ <linqherng2005@gmail.com>
2026-02-20 18:43:33 +0800

    add the name to the GUI window

--- src/main/java/javafx/Main.java
+++ src/main/java/javafx/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Qlin");
             // inject the Qlin instance
             fxmlLoader.<MainWindow>getController().setQlin(qlin);
             stage.show();
